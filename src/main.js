875cc9203418dbc9526d18890fda579fffcc2f2c
kadoshita <sublimer.me@gmail.com>
2019-11-30 16:12:14 +0900

    ファイルを追加

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow = null;
+
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, './static/index.html'));
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+};
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
