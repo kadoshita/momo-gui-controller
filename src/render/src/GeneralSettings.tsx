27f11d1a5ca205501889a8bdf5ce269f545daa75
kadoshita <sublimer.me@gmail.com>
2021-01-16 21:48:12 +0900

    add some form items

--- src/render/src/GeneralSettings.tsx
+++ src/render/src/GeneralSettings.tsx
@@ -1,23 +1,82 @@
 import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import { Button, Switch } from '@material-ui/core';
+import { Button, FormControl, FormControlLabel, Grid, InputLabel, MenuItem, NativeSelect, Select, Switch, TextField } from '@material-ui/core';
+
+const SwitchFormItem = (formProps: { name: string, control: any }) => {
+    return (
+        <Controller name={formProps.name} control={formProps.control} defaultValue={false}
+            render={props =>
+                <FormControlLabel
+                    control={<Switch onChange={e => props.onChange(e.target.checked)} checked={props.value} edge='end'></Switch>}
+                    label={formProps.name}
+                    labelPlacement='start'
+                ></FormControlLabel>
+            }
+        ></Controller>
+    )
+};
+
 const GeneralSettings = () => {
-    const { handleSubmit, control } = useForm<GeneralSettingsType>();
+    const { handleSubmit, control, register } = useForm<GeneralSettingsType>();
     const onSubmit = (data: any) => console.log(data);
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <Controller
-                name='noGoogleStun'
-                control={control}
-                defaultValue={false}
-                render={props =>
-                    <Switch
-                        onChange={e => props.onChange(e.target.checked)}
-                        checked={props.value}></Switch>
-                }
-            />
-
+            <Grid container spacing={3}>
+                <Grid item xs={6}>
+                    <SwitchFormItem name='noGoogleStun' control={control}></SwitchFormItem>
+                </Grid>
+                <Grid item xs={6}>
+                    <SwitchFormItem name='noVideoDevice' control={control}></SwitchFormItem>
+                </Grid>
+                <Grid item xs={6}>
+                    <SwitchFormItem name='noAudioDevice' control={control}></SwitchFormItem>
+                </Grid>
+                <Grid item xs={6}>
+                    <SwitchFormItem name='forceI420' control={control}></SwitchFormItem>
+                </Grid>
+                <Grid item xs={6}>
+                    <SwitchFormItem name='hwMjpegDecoder' control={control}></SwitchFormItem>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField inputRef={register} label='videoDevice' name='videoDevice' variant='outlined' fullWidth InputLabelProps={{ shrink: true }}></TextField>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField inputRef={register({ pattern: /(QVGA|VGA|HD|FHD|4K|\d+x\d+)/ })} label='resolution' name='resolution' fullWidth variant='outlined' placeholder='one of QVGA, VGA, HD, FHD, 4K, or [WIDTH]x[HEIGHT]' InputLabelProps={{ shrink: true }}></TextField>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField inputRef={register({ min: 1, max: 60 })} label='framerate' name='framerate' fullWidth variant='outlined' type='number' InputLabelProps={{ shrink: true }} inputProps={{ min: 1, max: 60 }}></TextField>
+                </Grid>
+                <Grid item xs={6}>
+                    <SwitchFormItem name='fixedResolution' control={control}></SwitchFormItem>
+                </Grid>
+                <Grid item xs={6}>
+                    <Controller name='priority' control={control} defaultValue='BALANCE'
+                        render={props =>
+                            <FormControl>
+                                <InputLabel shrink htmlFor="priority-select">priority</InputLabel>
+                                <Select
+                                    value={props.value}
+                                    onChange={props.onChange}
+                                    inputProps={{
+                                        name: 'priority',
+                                        id: 'priority-select',
+                                    }}
+                                >
+                                    <MenuItem value='BALANCE'>BALANCE</MenuItem>
+                                    <MenuItem value='FRAMERATE'>FRAMERATE</MenuItem>
+                                    <MenuItem value='RESOLUTION'>RESOLUTION</MenuItem>
+                                </Select>
+                            </FormControl>
+                        }></Controller>
+                </Grid>
+                <Grid item xs={6}>
+                    <SwitchFormItem name='useSdl' control={control}></SwitchFormItem>
+                </Grid>
+                <Grid item xs={6}>
+                    <SwitchFormItem name='showMe' control={control}></SwitchFormItem>
+                </Grid>
+            </Grid>
             <Button type='submit' fullWidth variant='contained' color='primary'>設定</Button>
         </form>
     )
